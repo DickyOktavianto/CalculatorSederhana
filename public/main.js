fc113da9505a68bcccd870e331c5750f6b74c7f1
DickyOktavianto <dickyoktavianto5@gmail.com>
2022-09-01 16:39:28 +0700

    3 files modified

--- public/main.js
+++ public/main.js
@@ -1,5 +1,3 @@
-// calculator
-
 let button = document.querySelector('.button');
 let btn = document.querySelectorAll('span');
 let value = document.getElementById('value');
@@ -7,10 +5,10 @@ let value = document.getElementById('value');
 for (let i = 0; i < btn.length; i++) {
   btn[i].addEventListener('click', function () {
     if (this.innerHTML == '=') {
-      value = innerHTML = eval(value.innerHTML);
+      value.innerHTML = eval(value.innerHTML);
     } else {
-      if (this.innerHTML == 'clear') {
-        value = this.innerHTML = '';
+      if (this.innerHTML == 'Clear') {
+        value.innerHTML = '';
       } else {
         value.innerHTML += this.innerHTML;
       }
