227d944abbe4adeb152ea710e02e3bdba451ee00
DickyOktavianto <dickyoktavianto5@gmail.com>
2022-09-01 11:21:02 +0700

    2 files modified

--- public/main.js
+++ public/main.js
@@ -1,3 +1,19 @@
-// run `node index.js` in the terminal
+// calculator
 
-console.log(`Hello Node.js`);
+let button = document.querySelector('button');
+let btn = document.querySelector('span');
+let value = document.querySelector('value');
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener('click', function () {
+    if (this.innerHTML == '=') {
+      value = innerHTML = eval(value.innerHTML);
+    } else {
+      if (this.innerHTML == 'clear') {
+        value = this.innerHTML = '';
+      } else {
+        value.innerHTML == this.innerHTML;
+      }
+    }
+  });
+}
